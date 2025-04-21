17b1ac14b6457f8617ae271d850b6f7792a26812
jun-young1993 <juny3738@gmail.com>
2025-04-21 15:45:11 +0900

    init

--- src/logger/middleware/request-logger.middleware.ts
+++ src/logger/middleware/request-logger.middleware.ts
@@ -1,23 +1,31 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Inject, Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
-
+import { WINSTON_MODULE_PROVIDER } from 'nest-winston';
+import { Logger } from 'winston';
 @Injectable()
 export class RequestLoggerMiddleware implements NestMiddleware {
+  constructor(
+    @Inject(WINSTON_MODULE_PROVIDER) private readonly logger: Logger,
+  ) {}
   use(req: Request, res: Response, next: NextFunction): void {
-    const { method, originalUrl, body, query, params } = req;
+    const { method, originalUrl, body, query, params, ip } = req;
     const startTime = Date.now();
-
+    const userAgent = req.get('user-agent') || '';
     // 요청 로깅
-    console.log(`[Request] ${method} ${originalUrl}`);
-    console.log('Query:', query);
-    console.log('Params:', params);
-    console.log('Body:', body);
+    this.logger.info('[REQUEST MIDDLEWARE] START');
+    this.logger.info(`[Request] ${method} ${originalUrl}`);
+    this.logger.info('Query:', query);
+    this.logger.info('Params:', params);
+    this.logger.info('Body:', body);
+    this.logger.info('IP:', ip);
+    this.logger.info('User-Agent:', userAgent);
+    this.logger.info('[REQUEST MIDDLEWARE] END');
 
     // 응답 로깅을 위한 이벤트 리스너
     res.on('finish', () => {
       const duration = Date.now() - startTime;
       const { statusCode } = res;
-      console.log(
+      this.logger.info(
         `[Response] ${method} ${originalUrl} ${statusCode} ${duration}ms`,
       );
     });
