87025f69b8170321de75145e9e8fc90073283f6b
jun-young1993 <juny3738@gmail.com>
2025-12-30 13:35:09 +0900

    add ignore git

--- src/aws/s3-object-migration.controller.ts
+++ src/aws/s3-object-migration.controller.ts
@@ -7,6 +7,8 @@ import { createNestLogger } from 'src/factories/logger.factory';
 import { calculateChecksum } from 'src/utils/stremes/check-sum';
 import { S3ObjectMetadataService } from './s3/s3-object-metadata.service';
 import { S3ObjectMetadata } from './s3/entities/s3-object-metadata.entity';
+import { CloudRunEmotionService } from 'src/cloud-run/emotion/cloud-run-emotion.service';
+import { CreateS3MetadataDto } from './s3/dto/create-s3-metadata.dto';
 
 @ApiTags('S3 Object Migration')
 @Controller('s3-object-migration')
@@ -15,6 +17,7 @@ export class S3ObjectMigrationController {
   constructor(
     private readonly awsS3Service: AwsS3Service,
     private readonly s3ObjectMetadataService: S3ObjectMetadataService,
+    private readonly cloudRunEmotionService: CloudRunEmotionService,
   ) {}
 
   @Post('checksum')
@@ -58,6 +61,57 @@ export class S3ObjectMigrationController {
     } catch (error) {
       console.log(error.stack);
 
+      this.logger.error(error);
+      throw new InternalServerErrorException(error);
+    }
+  }
+
+  @Post('caption')
+  @ApiOperation({ summary: 'Migrate S3 Object Caption' })
+  async migrateS3ObjectCaption() {
+    try {
+      const s3ObjectList = await this.awsS3Service.getObjects({
+        where: [
+          {
+            metadata: {
+              caption: IsNull(),
+            },
+            destination: S3ObjectDestinationType.UPLOAD,
+          },
+        ],
+        relations: ['metadata'],
+      });
+      if (s3ObjectList.length === 0) {
+        return;
+      }
+      for (const s3Object of s3ObjectList) {
+        try {
+          if (s3Object.metadata && s3Object.metadata.caption === null) {
+            const caption = await this.cloudRunEmotionService.imageToCaption(
+              s3Object.url,
+            );
+            s3Object.metadata.caption = caption.text;
+
+            console.log('[created caption]', caption.text);
+            await this.s3ObjectMetadataService.update(s3Object.metadata);
+          } else {
+            const caption = await this.cloudRunEmotionService.imageToCaption(
+              s3Object.url,
+            );
+            const metadata = await this.s3ObjectMetadataService.create(
+              CreateS3MetadataDto.fromJson({
+                s3Object: s3Object,
+                caption: caption.text,
+              }),
+            );
+            console.log('[created caption]', metadata.caption);
+          }
+        } catch (error) {
+          this.logger.error(error);
+          continue;
+        }
+      }
+    } catch (error) {
       this.logger.error(error);
       throw new InternalServerErrorException(error);
     }
